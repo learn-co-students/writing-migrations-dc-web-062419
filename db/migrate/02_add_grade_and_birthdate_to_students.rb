b64b1319cbcb40c8baa28a05c393812f506de7ec
SkaMateria <skamateria@gmail.com>
2019-07-09 18:39:54 -0400

    Done.

---
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -0,0 +1,7 @@
+class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.1]
+    
+    def change 
+        add_column :students, :grade, :date
+        add_column :students, :birthdate, :string
+    end
+end
